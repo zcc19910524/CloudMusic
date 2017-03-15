ef35f4daa174764f2bd356e125dbc5fb261dd4ed
zhaochenchen <ialwayswannafly@yahoo.com>
2017-03-15 21:31:27 +0800

    cloudmusic

--- src/component/Playing.js
+++ src/component/Playing.js
@@ -14,6 +14,16 @@ export default class Playing extends Component{
                          <i className="iconfont icon-bianji"></i>
                     </span>
                 </div>
+                <div className="player">
+
+                </div>
+                <div className="controller">
+                    <i className="iconfont icon-danquxunhuan"></i>
+                    <i className="iconfont icon-previous"></i>
+                    <i className="iconfont icon-pause"></i>
+                    <i className="iconfont icon-xiayiqu"></i>
+                    <i className="iconfont icon-2"></i>
+                </div>
             </div>
         )
     }
